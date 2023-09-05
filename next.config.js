dd11c01d314f5784a39fa9619c935fc2afba22fb
Nesar Ahmed <nesar@fcub.edu.bd>
2023-09-05 16:10:23 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: "edge"
-  },
+  runtime: 'experimental-edge',
   images: {
     domains: [
       "www.facebook.com",
